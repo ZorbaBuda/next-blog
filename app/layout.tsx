1d5443e632bee50d5212b1b9872dbf2d8b7f4701
Fran <fm-a@robindatahood.com>
2023-12-06 17:37:35 +0100

    renamed scss with module

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.scss";
+import "./globals.module.scss";
 import config from "@/data/config.json";
 import type { Metadata } from "next";
 import { ThemeProviders } from "./theme-providers";
